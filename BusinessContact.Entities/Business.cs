a692b7eb53b10d326790c1eb96d36b1df2b2124e
abelsaul <abelmiraval@gmail.com>
2022-10-25 16:36:13 -0500

    Add required attributes

--- BusinessContact.Entities/Business.cs
+++ BusinessContact.Entities/Business.cs
@@ -1,11 +1,19 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace BusinessContact.Entities
 {
     public class Business : EntityBase
     {
+        [Required]
+        [StringLength(11)]
         public string Number { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Address { get; set; }
+        [Required]
+        public int DistrictId { get; set; }
         public virtual District District { get; set; }
 
     }
