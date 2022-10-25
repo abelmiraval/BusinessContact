36d83ce93f5194081d674a5848addb6aa6cfbe00
abelsaul <abelmiraval@gmail.com>
2022-10-25 16:36:30 -0500

    Add constants

---
+++ BusinessContact.Services/Util/Constants/ErrorConstant.cs
@@ -0,0 +1,7 @@
+﻿namespace BusinessContact.Services.Util.Constants
+{
+    public static class ErrorConstant
+    {
+        public const string Message = "Hubo un error al procesar la información";
+    }
+}
